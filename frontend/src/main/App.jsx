bc96f59b6633bfb04c08d932b4c656910499049e
FidFenix <fid.mamani@gmail.com>
2020-08-07 04:54:30 -0500

    adding gitignore file

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from React;
+import 'App.css';
+
+class App extends Component {
+   constructor() {
+      super();
+      this.state= {
+
+      }
+   }
+   render() {
+      return(
+         <h1>Hello API</h1>
+      )
+   }
+}
+
+export default App;
