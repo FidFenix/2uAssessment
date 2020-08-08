02770013c49126a772bf8ed05ce87d06288e0b32
FidFenix <fid.mamani@gmail.com>
2020-08-08 13:34:35 -0500

    Changing DB conexion AWS is slow

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,10 @@
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'fidelito2U',
-    database: 'twou',
-    host: 'twou.c5doapu0dfmp.us-east-2.rds.amazonaws.com',
-    dialect: 'postgres',
-    port: '5432'
+    username: 'zbcdchwb',
+    password: 'RlAxYu5xKsvwDWDOqMeKp-2diRUHK1wj',
+    database: 'zbcdchwb',
+    host: 'rajje.db.elephantsql.com',
+    dialect: 'postgres'
   },
   production: process.env.DATABASE_URL
 };
